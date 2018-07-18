dc6ac873cc0bbfb2de54ed7e8be6b77790468343
Lai Yingxiao <laiyingxiao@zhihu.com>
2018-07-18 19:34:23 +0800

    book, author and details

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -19,5 +19,50 @@ def index(request):
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,
+                 'num_authors':num_authors},
     )
+
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    # 这个 model 值的字面很重要
+    # the generic views look for templates in /application_name/the_model_name_list.html (catalog/book_list.html in this case)
+    # inside the application's /application_name/templates/ directory (/catalog/templates/).
+    model = Book
+
+    paginate_by = 3
+
+    def get_queryset(self):
+        # return Book.objects.filter(title__icontains='war')[:5]  # Get 5 books containing the title war
+        return Book.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+    # context_object_name = 'my_book_list'  # your own name for the list as a template variable
+    # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+    def get_queryset(self):
+        return Author.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        return super(AuthorListView, self).get_context_data(**kwargs)
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
